d0d17783d6b9621acb7aaa6c6736321b641b48be
Caio Aureliano <caio.aureliano1@gmail.com>
2022-07-06 12:42:04 -0300

    test: create url short with status, location response '

--- internal/shortener/handler/handler_test.go
+++ internal/shortener/handler/handler_test.go
@@ -1,12 +1,12 @@
 package handler
 
 import (
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
+	"github.com/CaioAureliano/gortener/internal/shortener/dto"
 	"github.com/CaioAureliano/gortener/internal/shortener/model"
 	"github.com/CaioAureliano/gortener/internal/shortener/service"
 	"github.com/labstack/echo/v4"
@@ -24,10 +24,15 @@ func TestCreateShortUrl(t *testing.T) {
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 
+	slugMock := "sl8g3"
 	shortenerService = func() service.Shortener {
 		return mockService{
-			fnCreate: func(url string) (*model.Shortener, error) {
-				return &model.Shortener{Url: url}, nil
+			fnCreate: func(req *dto.UrlRequest) (*model.Shortener, error) {
+				req.AppendProtocolIfNotExists()
+				return &model.Shortener{
+					Url:  req.Url,
+					Slug: slugMock,
+				}, nil
 			},
 		}
 	}
@@ -35,10 +40,8 @@ func TestCreateShortUrl(t *testing.T) {
 	err := CreateShortUrl(c)
 
 	if assert.NoError(t, err) {
-		log.Printf("body response: %v", rec.Body.String())
-		log.Printf("headers response: %v", rec.Header())
-
 		assert.Equal(t, http.StatusCreated, rec.Code)
 		assert.Equal(t, echo.MIMEApplicationJSONCharsetUTF8, rec.Header()[echo.HeaderContentType][0])
+		assert.Equal(t, "/"+slugMock, rec.Header()[echo.HeaderLocation][0])
 	}
 }
--- internal/shortener/handler/mocks_test.go
+++ internal/shortener/handler/mocks_test.go
@@ -1,18 +1,21 @@
 package handler
 
-import "github.com/CaioAureliano/gortener/internal/shortener/model"
+import (
+	"github.com/CaioAureliano/gortener/internal/shortener/dto"
+	"github.com/CaioAureliano/gortener/internal/shortener/model"
+)
 
 type mockService struct {
-	fnCreate   func(url string) (*model.Shortener, error)
+	fnCreate   func(req *dto.UrlRequest) (*model.Shortener, error)
 	fnGet      func(slug string) (*model.Shortener, error)
 	fnGetUrl   func(slug string) (string, error)
 	fnAddClick func(click model.Click, slug string) (*model.Shortener, error)
 	fnStats    func(slug string) (*model.Stats, error)
 }
 
-func (m mockService) Create(url string) (*model.Shortener, error) {
+func (m mockService) Create(req *dto.UrlRequest) (*model.Shortener, error) {
 	if m.fnCreate != nil {
-		return m.fnCreate(url)
+		return m.fnCreate(req)
 	}
 	return nil, nil
 }
